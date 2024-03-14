e510a524a95b4b7c1c66b0939e822dbf4025e94d
187QW187QW <1416419442@qq.com>
2024-03-14 22:39:33 +0800

    finish

--- dag.go
+++ dag.go
@@ -5,11 +5,6 @@ import (
 	"hash"
 )
 
-type Tree struct {
-	Hash []byte
-	Data []byte
-}
-
 type Link struct {
 	Name string
 	Hash []byte
@@ -31,128 +26,114 @@ const (
 // 参数node是要添加的节点
 // 参数h是哈希函数实例
 func Add(store KVStore, node Node, h hash.Hash) []byte {
-	switch n := node.(type) {
+	switch node.(type) {
 	case File:
-		return addFile(store, n, h)
+		file := node.(File)
+		addFile(store, file, h)
 	case Dir:
-		return addDir(store, n, h)
-	default:
-		return nil
+		dir := node.(Dir)
+		addDir(store, dir, h)
 	}
+	return h.Sum(nil)
 }
 
 // addFile函数用于将文件节点添加到Merkle Dag中
-func addFile(store KVStore, file File, h hash.Hash) []byte {
+func addFile(store KVStore, file File, h hash.Hash) *Object {
 	data := file.Bytes()
 	if len(data) <= Block_Size {
 		// 如果文件大小不超过256KB，则直接将文件内容写入KVStore，并返回哈希值
-		h.Write(data)
-		hashValue := h.Sum(nil)
-		err := store.Put(hashValue, data)
-		if err != nil {
-			return nil
+		blob := Object{
+			Links: nil,
+			Data:  data,
 		}
-		return hashValue
+		jsonMarshal, _ := json.Marshal(blob)
+		h.Write(jsonMarshal)
+		store.Put(h.Sum(nil), data)
+		return &blob
+	} else {
+		res := dealFile_(file, store, h)
+		return res
 	}
-
-	// 如果文件大小超过256KB，则进行分片存储
-	chunks := splitDataIntoChunks(data, Block_Size)
-	var chunkHashes [][]byte
-
-	// 遍历每个分片，计算哈希值，并将哈希值和分片数据保存在KVStore中
-	for _, chunk := range chunks {
-		h.Write(chunk)
-		hashValue := h.Sum(nil)
-		err := store.Put(hashValue, chunk)
-		if err != nil {
-			return nil
-		}
-		chunkHashes = append(chunkHashes, hashValue)
-		h.Reset()
-	}
-
-	// 将所有分片的哈希值拼接成一个连续的字节切片
-	concatenated := concatenateHashes(chunkHashes)
-	h.Write(concatenated)
-
-	// 计算并返回Merkle Root的哈希值
-	merkleRoot := h.Sum(nil)
-	return merkleRoot
 }
 
 // addDir函数用于将文件夹节点添加到Merkle Dag中
-func addDir(store KVStore, dir Dir, h hash.Hash) []byte {
+func addDir(store KVStore, dir Dir, h hash.Hash) *Object {
 	it := dir.It()
-	var links []Link
+	treesObject := &Object{}
 
 	// 遍历文件夹中的文件和文件夹
 	for it.Next() {
 		node := it.Node()
 
-		switch n := node.(type) {
+		switch node.(type) {
 		case File:
 			// 如果是文件节点，则递归调用addFile函数，并保存返回的哈希值和文件名
-			hashValue := addFile(store, n, h)
-			link := Link{
-				Name: n.Name(),
-				Hash: hashValue,
-				Size: int(n.Size()),
+			file := node.(File)
+			tmp := addFile(store, file, h)
+			jsonMarshal, _ := json.Marshal(tmp)
+			h.Write(jsonMarshal)
+			treesObject.Links = append(treesObject.Links, Link{
+				Hash: h.Sum(nil),
+				Size: int(file.Size()),
+				Name: file.Name(),
+			})
+			typeName := "link"
+			if tmp.Links == nil {
+				typeName = "blob"
 			}
-			links = append(links, link)
+			treesObject.Data = append(treesObject.Data, []byte(typeName)...)
+
 		case Dir:
 			// 如果是文件夹节点，则递归调用addDir函数，并保存返回的哈希值和文件夹名
-			hashValue := addDir(store, n, h)
-			link := Link{
-				Name: n.Name(),
-				Hash: hashValue,
-				Size: int(n.Size()),
-			}
-			links = append(links, link)
+			dir := node.(Dir)
+			tmp := addDir(store, dir, h)
+			jsonMarshal, _ := json.Marshal(tmp)
+			h.Write(jsonMarshal)
+			treesObject.Links = append(treesObject.Links, Link{
+				Hash: h.Sum(nil),
+				Size: int(dir.Size()),
+				Name: dir.Name(),
+			})
+			typeName := "tree"
+			treesObject.Data = append(treesObject.Data, []byte(typeName)...)
 		}
 	}
-
-	// 将所有子节点的哈希值和名称组成一个Object
-	obj := Object{
-		Links: links,
-		Data:  nil,
-	}
-
-	// 将Object序列化为JSON
-	jsonData, err := json.Marshal(obj)
-	if err != nil {
-		return nil
-	}
-
-	// 计算JSON的哈希值作为Merkle Root，并将JSON数据保存在KVStore中
-	h.Write(jsonData)
-	merkleRoot := h.Sum(nil)
-	err = store.Put(merkleRoot, jsonData)
-	if err != nil {
-		return nil
-	}
-
-	return merkleRoot
+	jsonMarshal, _ := json.Marshal(treesObject)
+	h.Write(jsonMarshal)
+	store.Put(h.Sum(nil), jsonMarshal)
+	return treesObject
 }
 
-// splitDataIntoChunks函数用于将数据分成指定大小的块
-func splitDataIntoChunks(data []byte, chunkSize int) [][]byte {
-	var chunks [][]byte
-	for i := 0; i < len(data); i += chunkSize {
-		end := i + chunkSize
-		if end > len(data) {
-			end = len(data)
+func dealFile_(node File, store KVStore, h hash.Hash) *Object {
+	links := &Object{}
+	for i := 0; i < int(node.Size()/256)+1; i += 256 { //有几个256 分多一块
+		//分片  需要link
+		//end 每次 + 256  最后一片不满256  end = node.Size()
+		//data 取 [i:end]
+		end := i + 256
+		if int(node.Size()) < end {
+			end = int(node.Size())
 		}
-		chunk := data[i:end]
-		chunks = append(chunks, chunk)
-	}
-	return chunks
-}
+		data := node.Bytes()[i:end]
+		blob := Object{
+			Links: nil,
+			Data:  data,
+		}
+		jsonMarshal, _ := json.Marshal(blob)
+		h.Write(jsonMarshal)
+		store.Put(h.Sum(nil), data)
 
-// concatenateHashes函数用于将哈希值拼接成一个连续的字节切片。
-func concatenateHashes(hashes [][]byte) []byte {
-	var concatenated []byte
-	for _, hashValue := range hashes {
-		concatenated = append(concatenated, hashValue...)
+		//分片写入links 每个ipfs就代指一片
+		links.Links = append(links.Links, Link{
+			Name: node.Name(),
+			Hash: h.Sum(nil),
+			Size: len(data),
+		})
+		links.Data = append(links.Data, []byte("blob")...)
 	}
-	return concatenated
+	//links写入KVStore
+	jsonMarshal, _ := json.Marshal(links)
+	h.Write(jsonMarshal)
+	store.Put(h.Sum(nil), jsonMarshal)
+	return links
 }
